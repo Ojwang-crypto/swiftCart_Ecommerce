593e525e7b14c937cbcb9091375c770813a8452b
Ojwang-crypto <omondicollins163@gmail.com>
2023-03-27 22:59:44 +0300

    destructured divs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import './categories.styles.scss'
+const App = () => {
+  const categories = [
+    {
+      "id": 1,
+      "title": "hats",
+      "imageUrl": 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+    },
+    {
+      "id": 2,
+      "title": "jackets",
+      "imageUrl": 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
+    },
+    {
+      "id": 3,
+      "title": "sneakers",
+      "imageUrl": 'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg',
+    },
+    {
+      "id": 4,
+      "title": "womens",
+      "imageUrl": 'https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg',
+    },
+    {
+      "id": 5,
+      "title": "mens",
+      "imageUrl": 'https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg',
+    }
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-container">
+    {categories.map(({title, id, imageUrl}) => (
+      <div key={id} className="category-container" >
+          <div className="background-image" style={{ backgroundImage: `url(${imageUrl})`, }}/>
+          <div className="category-body-container">
+              <h2>{title}</h2>
+              <p>Shop Now</p>
+          </div>
+
+      </div> 
+     ))}        
     </div>
   );
 }
